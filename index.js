1658c9cd2f58e001cd656cf2d0f0f729b9d00227
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-03-27 01:51:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const __path = process.cwd();
 const PORT = process.env.PORT || 8000;
-
+require("./express.js")
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
